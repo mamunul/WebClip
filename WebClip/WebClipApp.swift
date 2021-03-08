aa905d4c6dae29c4f1e0b373c7fa82e332437bfd
MamunulMazid <mamunul.mazid@gmail.com>
2021-03-08 11:18:01 +0600

    Initial Commit

---
+++ WebClip/WebClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebClipApp.swift
+//  WebClip
+//
+//  Created by Mamunul Mazid on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WebClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
